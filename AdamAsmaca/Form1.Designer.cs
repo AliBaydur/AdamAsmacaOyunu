992377ce297cfb1710af853b27e8dc5395aa27b7
AliBaydur <59739580+AliBaydur@github.com>
2022-04-21 17:36:55 +0300

    Add project files.

---
+++ AdamAsmaca/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace AdamAsmaca
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pckb_AdamAs = new System.Windows.Forms.PictureBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.lbl_mesaj = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pckb_AdamAs)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pckb_AdamAs
+            // 
+            this.pckb_AdamAs.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pckb_AdamAs.Location = new System.Drawing.Point(812, 46);
+            this.pckb_AdamAs.Name = "pckb_AdamAs";
+            this.pckb_AdamAs.Size = new System.Drawing.Size(288, 329);
+            this.pckb_AdamAs.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pckb_AdamAs.TabIndex = 0;
+            this.pckb_AdamAs.TabStop = false;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1112, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // lbl_mesaj
+            // 
+            this.lbl_mesaj.AutoSize = true;
+            this.lbl_mesaj.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_mesaj.Location = new System.Drawing.Point(412, 58);
+            this.lbl_mesaj.Name = "lbl_mesaj";
+            this.lbl_mesaj.Size = new System.Drawing.Size(0, 14);
+            this.lbl_mesaj.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1112, 450);
+            this.Controls.Add(this.lbl_mesaj);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.pckb_AdamAs);
+            this.Name = "Form1";
+            this.Text = "Adam Asmaca Oyunu ";
+            ((System.ComponentModel.ISupportInitialize)(this.pckb_AdamAs)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pckb_AdamAs;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.Label lbl_mesaj;
+    }
+}
+
---
+++ AdamAsmaca/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdamAsmaca
+{
+    public partial class Form1 : Form
+    {
+        string secilenkelime = "";
+        Label[] labels;
+        int say = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            pckb_AdamAs.ImageLocation = @"..\..\Images\01.png";
+            pckb_AdamAs.Load();
+            ButtonAdd();
+            LabelAdd();
+        }
+
+        private void LabelAdd()
+        {
+            string[] kelimeler = new string[] {"TÜRKİYE", "FRANSA", "ALMANYA", "İSPANYA", "İTALYA", "LÜKSEMBURG",
+                "HOLLANDA", "İSVİÇRE", "BELÇİKA", "AVUSTURYA", "RUSYA", "SLOVAKYA", "ROMANYA", "UKRAYNA", "DANİMARKA",
+                "ESTONYA", "LİTVANYA", "HIRVATİSTAN", "NORVEÇ", "FİNLANDİYA", "İSVEÇ", "ARNAVUTLUK", "LETONYA", "ANDORRA",
+                "KARADAĞ", "MAKEDONYA", "YUNANİSTAN", "KUZEYKIBRIS", "SLOVENYA", "SIRBİSTAN", "SANMARİNO", "MALTA", "KOSOVA",
+                "PORTEKİZ", "GÜNEYOSETYA", "BULGARİSTAN", "RUSYA","SURİYE", "İRAN", "AFGANİSTAN", "ERMENİSTAN", "AZERBAYCAN",
+                "JAPONYA", "ÇİN", "IRAK", "HİNDİSTAN", "MALDİVLER", "PAKİSTAN", "MOĞOLİSTAN", "NEPAL", "ARABİSTAN", "TAYLAND",
+                "ÜRDÜN", "GÜRCİSTAN", "LÜBNAN", "VİETNAM", "KUZEYKORE", "FİLİPİNLER", "GÜNEYKORE", "MALEZYA", "TAYVAN", "KATAR",
+                "KAMBOÇYA", "KANADA", "BREZİLYA", "AMERİKA", "JAMAİKA", "MEKSİKA", "GRANADA", "ARJANTİN", "URUGUAY", "ŞİLİİ", "PERU",
+                "MISIR", "KENYA", "FAS", "GANA", "CEZAİR", "BENİN", "GİNA", "TUNUS", "GANA", "YEŞİLBURUN", "AVUSTURYA", "FİJİ", "ENDONEZYA",
+                "PALAU", "KİRİBATİ"};
+
+            Random rnd = new Random();
+            secilenkelime = kelimeler[rnd.Next(kelimeler.Length)];
+            labels = new Label[secilenkelime.Length];
+            int x = 300, y = 150;
+
+            for (int i = 0; i < secilenkelime.Length; i++)
+            {
+                Label lbl = new Label();
+                lbl.Name = "lbl_" + secilenkelime[i];
+                lbl.Text = "_";
+                lbl.Size = new Size(40, 40);
+                lbl.Location = new Point(x, y);
+                lbl.Font = new Font("Arial", 24, FontStyle.Bold);
+                this.Controls.Add(lbl);
+                labels[i] = lbl;
+                labels[i].Text = "_";
+
+                x += 40;
+            }
+        }
+
+        private void ButtonAdd()
+        {
+            string[] harfler = new string[] {"A", "B", "C", "Ç", "D", "E", "F", "G", "Ğ", "H", "I", "İ", "J", "K", "L", "M",
+             "N", "O", "Ö", "P", "Q", "R", "S", "Ş", "T", "U", "Ü", "V", "W", "X", "Y", "Z"};
+
+            int x = 0, y = 0;
+
+            for (int i = 0; i < harfler.Length; i++)
+            {
+                Button btn = new Button();
+                btn.Name = "btn_" + harfler[i];
+                btn.Text = harfler[i].ToString();
+                btn.Click += new EventHandler(button_Click);
+                btn.Size = new Size(40, 40);
+
+                if (i % 5 == 0)
+                {
+                    x = 40; y += 40;
+                }
+                else
+                {
+                    x += 40;
+                }
+                btn.Location = new Point(x, y);
+                this.Controls.Add(btn);
+            }
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            btn.Enabled = false;
+            bool buldumu = false;
+
+            if (say <= 6)
+            {
+                for (int i = 0; i < secilenkelime.Length; i++)
+                {
+                    if (btn.Text == secilenkelime[i].ToString())
+                    {
+                        labels[i].Text = btn.Text;
+                        buldumu = true;
+                        lbl_mesaj.Text = "Süpersin Dostum.:)";
+                    }
+                    else
+                    {
+                        lbl_mesaj.Text = "Opps.. İyi Düşün Dostum Hangi Ülke Bu .;)";
+                    }
+                }
+            }
+            else
+            {
+                lbl_mesaj.Text = "Malesef Kaybettin Dostum. Bir Daha Dene Şansını..:)";
+            }
+
+            if (buldumu == false)
+            {
+                say++;
+                AdamAs();
+
+            }
+        }
+
+        private void AdamAs()
+        {
+            switch (say)
+            {
+                case 1:
+                    pckb_AdamAs.ImageLocation = @"..\..\Images\01.png";
+                    pckb_AdamAs.Load();
+                    break;
+                case 2:
+                    pckb_AdamAs.ImageLocation = @"..\..\Images\02.png";
+                    pckb_AdamAs.Load();
+                    break;
+                case 3:
+                    pckb_AdamAs.ImageLocation = @"..\..\Images\03.png";
+                    pckb_AdamAs.Load();
+                    break;
+                case 4:
+                    pckb_AdamAs.ImageLocation = @"..\..\Images\04.png";
+                    pckb_AdamAs.Load();
+                    break;
+                case 5:
+                    pckb_AdamAs.ImageLocation = @"..\..\Images\05.png";
+                    pckb_AdamAs.Load();
+                    break;
+                case 6:
+                    pckb_AdamAs.ImageLocation = @"..\..\Images\06.png";
+                    pckb_AdamAs.Load();
+                    break;
+            }
+        }
+
+    }
+}
